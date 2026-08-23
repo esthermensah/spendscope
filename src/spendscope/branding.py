3d3a5c5e9d4927d1534fa6b39228e46616c680e4
Esther Mensah <46500417+esthermensah@users.noreply.github.com>
2026-08-23 19:21:36 -0400

    Release beta 6 invoice OCR fix

--- src/spendscope/branding.py
+++ src/spendscope/branding.py
@@ -2,6 +2,6 @@
 
 PRODUCT_NAME = "SpendScope"
 APP_ID = "org.spendscope.desktop"
-VERSION = "0.1.0-beta.5"
+VERSION = "0.1.0-beta.6"
 DEFAULT_REPORT_TITLE = f"{PRODUCT_NAME} Spending Report"
 SUPPORT_URL = "https://github.com/esthermensah/spendscope"
